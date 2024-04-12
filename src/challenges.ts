1e9bc90d99655e0d7122a800cd12648dd9329678
ekaterina.shevchenko <myworkk09@gmail.com>
2024-04-12 16:25:55 +0300

    1,2 challenges

---
+++ src/challenges.ts
@@ -0,0 +1,36 @@
+//1. Pick
+// interface Todo {
+// 	title: string,
+// 	// description: string,
+// 	completed: boolean
+// }
+//
+// type MyPick<T, K extends keyof T> = { [Key in K]: T[Key] }
+//
+// type TodoPreview = MyPick<Todo, 'title' | 'completed'>
+//
+// const todo: TodoPreview = {
+// 	title: 'gg',
+// 	completed: true
+// }
+// todo
+
+
+//2. ReadOnly
+
+// type MyReadonly<T> = { readonly [Key in keyof T]: T[Key]}
+//
+// const todo: MyReadonly<Todo> = {
+// 	title: 'cat',
+// 	completed: false
+// }
+//
+// todo.title = dog
+
+//3. Tuple to Object
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+type TupleToObject<A extends any[]> = { [I in A]:}
+
+type result = TupleToObject<typeof tuple>
